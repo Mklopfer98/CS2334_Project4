8d3d862ace682fe57e012f33a540266622e7b4a0
Matthew Klopfer <klopfermatthew@gmail.com>
2018-12-05 09:26:47 -0600

    Table panel model looking correct

--- TabelPanel.java
+++ TabelPanel.java
@@ -7,20 +7,28 @@ public class TabelPanel extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
 	
-	/** Panel to hold the table **/
-	JPanel contain = new JPanel();
-	
 	/** JTable to hold the values the user wishes to display **/
-	JTable table = new JTable();
+	private JTable table = new JTable(100, 6);
 	
+	/** Create the table column values **/
+	private String[] columNames = {"Station", "Parameter", "Statistics", "Value", "Reporting Stations", "Data"};
+	
+	/** Create scroll pane **/
+	private JScrollPane scroll = new JScrollPane(table);
 
 	public TabelPanel() {
+		
+		// Set the headers of the table
+		for (int i = 0; i < columNames.length; ++i) {
+			table.getColumnModel().getColumn(i).setHeaderValue(columNames[i]);
+		}
+		
 		setLayout(new BorderLayout());
 		table.setFont(MesonetFrame.timesRoman);
-		contain.setBackground(Color.WHITE);
+		table.setBackground(Color.WHITE);
 		
-		contain.add(table);
-		add(contain);
+		
+		add(scroll);
 		
 	}
 }
